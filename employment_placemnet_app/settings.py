58bd2c75d7ac0af82c6f7ee9267b15b5cfa7db65
Oscar Constantino <12437469+OscarConstantino@users.noreply.github.com>
2025-06-14 09:43:16 -0600

    Update settings.py

--- employment_placemnet_app/settings.py
+++ employment_placemnet_app/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 from django.contrib.messages import constants as messages
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
