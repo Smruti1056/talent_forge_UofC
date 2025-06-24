a11baa052ea5f531b6079fd0636776344769972b
Oscar Constantino <oscar.constmota@gmail.com>
2025-06-24 09:26:33 -0600

    Uncommenting STATIC_URL parameter

--- employment_placemnet_app/settings.py
+++ employment_placemnet_app/settings.py
@@ -157,7 +157,7 @@ DOMAIN = 'http://localhost:8000'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 # Tell Django where to put “collected” static files
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / "static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
